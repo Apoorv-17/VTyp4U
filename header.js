1793fe4f4571cbd359d112dc5d2569b16e4dd85e
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-08-30 22:23:48 +0530

    fixing all urls

--- header.js
+++ header.js
@@ -2,11 +2,11 @@ var c=0;
 function navg(){
     if(c==0)
     {
-      document.getElementById('lg').src  = 'https://drive.google.com/file/d/1zi4colO0oyxJABzH9TxV15Fy5lobrHOQ/view?usp=sharing';
+      document.getElementById('lg').src  = 'https://lh3.googleusercontent.com/0f27k6eG9bTve9x_RVJVs1gW86p6YrtYtvNitWem3C79mE0q2L13RnFzuuScxGe97qInbTYKmHbW0t-7f3MMl2sQjsZ05eA3ru5FU3vREsWj5Tt_7kZluQk3-yrrCIjTAwLzvg4GYA';
       c=1;
     }
     else{
-     document.getElementById('lg').src  = 'https://drive.google.com/file/d/1kxBOUnJjjQ280oc9j4tlF4r1iMdJBsjc/view?usp=sharing';
+     document.getElementById('lg').src  = 'https://lh3.googleusercontent.com/1gDPitodvW9cXzwpFvQBH1r3nd3QP80NU-XEtMQdCRNejbKZT_T_JOsX5MgeOX5moJfwHNtvBkgMCHgcCH1hhslaQv6kKavyFDVZBTpc_aWJEIxPODWI1DhRx975M_TaKDJ4VtHuhQ';
      c=0;
     }
     document.getElementById("header").classList.toggle("header-light");
