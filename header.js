a35d4d0890c2cc67d11a2b90c1fe1ddd8221bb5f
Shush25 <shushant.kumar.singh.5@gmail.com>
2020-08-30 16:59:31 +0530

    changed img urls

--- header.js
+++ header.js
@@ -2,11 +2,11 @@ var c=0;
 function navg(){
     if(c==0)
     {
-      document.getElementById('lg').src  = 'logolight.png';
+      document.getElementById('lg').src  = 'https://drive.google.com/file/d/1zi4colO0oyxJABzH9TxV15Fy5lobrHOQ/view?usp=sharing';
       c=1;
     }
     else{
-     document.getElementById('lg').src  = 'logobeta.png';
+     document.getElementById('lg').src  = 'https://drive.google.com/file/d/1kxBOUnJjjQ280oc9j4tlF4r1iMdJBsjc/view?usp=sharing';
      c=0;
     }
     document.getElementById("header").classList.toggle("header-light");
